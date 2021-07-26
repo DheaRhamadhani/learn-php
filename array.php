cf1763dd3f63c4c0320410de4775b2aa4dbd26f2
DheaRhamadhani <DheaRhamadhani@gmail.com>
2021-07-26 12:21:36 +0700

    Upload Pertama

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * array adalah kumpulan nilai yg berjenis sama
+ * 
+ * variabel -> individu
+ * array -> populasi
+ */
+$student = "Bill Gates";
+$siswa = ["John Cena","The Rock","Batista","Rey Mysterio"];
+echo $siswa[1]
+?>
